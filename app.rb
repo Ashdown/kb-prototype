34a63c5960be3666a165b8b97b31010e4051304e
Rory Devane <rory.devane@gmail.com>
2017-10-15 01:49:21 +0100

    added main nav html and empty pages

--- app.rb
+++ app.rb
@@ -2,5 +2,25 @@ require 'sinatra'
 require 'slim'
 
 get '/' do
-  erb :index
+  erb :home
+end
+
+get '/newsletter' do
+  erb :newsletter
+end
+
+get '/campaign' do
+  erb :campaign
+end
+
+get '/offering-help' do
+  erb :offering_help
+end
+
+get '/needing-help' do
+  erb :needing_help
+end
+
+get '/links' do
+  erb :links
 end
